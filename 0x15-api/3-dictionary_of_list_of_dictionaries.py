b5a3445ea5a7ac1fdf9ad1d37e7cdfc9815fc68d
Tony Sims <zocle@zocle.net>
2020-09-14 23:20:30 -0500

    reverting to static eid count

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,29 +7,34 @@ import json
 
 if __name__ == "__main__":
     ''' main '''
+    eid = 1
     url = "https://jsonplaceholder.typicode.com/users/"
     todict = {}
-    todo_list = []
+
     try:
-        users = requests.get(url).json()
-        for user in users:
-            eid = user.get('id')
-            username = user.get('username')
-            todos = requests.get(
-                "{}{}/todos/".format(url, eid)).json()
+        max_id = len(requests.get(url).json())
+        while eid <= max_id:
+            username = requests.get(
+                "{}{}".format(url, eid)).json().get("username")
+            if username:
+                todos = requests.get(
+                    "{}{}/todos/".format(url, eid)).json()
+                todo_list = []
 
-            for todo in todos:
-                todo_list.append(
-                    {
-                        "username": username,
-                        "task": todo.get('title'),
-                        "completed": todo.get('completed')
-                    }
-                )
-            todict[eid] = todo_list
+                for todo in todos:
+                    todo_list.append(
+                        {
+                            "username": username,
+                            "task": todo.get('title'),
+                            "completed": todo.get('completed')
+                        }
+                    )
+                todict[eid] = todo_list
 
-            with open("todo_all_employees.json", 'w') as jsonfile:
-                json.dump(todict, jsonfile)
+                with open("todo_all_employees.json", 'w') as jsonfile:
+                    json.dump(todict, jsonfile)
+
+            eid += 1
 
     except:
         pass
