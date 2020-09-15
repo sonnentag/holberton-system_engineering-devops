3b51f0c63a8017095ce1b063e39b5206279461dc
Tony Sims <zocle@zocle.net>
2020-09-14 21:14:36 -0500

    additional formatting for named input vars

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     ''' main '''
     try:
         eid = sys.argv[1]
-        url = "https://jsonplaceholder.typicode.com/users/" + eid
+        url = "https://jsonplaceholder.typicode.com/users/{}".format(eid)
         name = requests.get(url).json().get("name")
         if name:
-            num_todos = len(requests.get(url + "/todos/").json())
-            completed = requests.get(url + "/todos/",
+            num_todos = len(requests.get(url).json())
+            completed = requests.get("{}/todos/".format(url),
                                      params={"completed": 'true'}).json()
 
             print("Employee {} is done with tasks({}/{}):"
