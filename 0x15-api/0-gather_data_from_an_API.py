12a73be4e2ef5cd7aaad04eb90f09da4f9725e6b
Tony Sims <zocle@zocle.net>
2020-09-14 21:22:45 -0500

    reverted additional formatting

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
     ''' main '''
     try:
         eid = sys.argv[1]
-        url = "https://jsonplaceholder.typicode.com/users/{}".format(eid)
+        url = "https://jsonplaceholder.typicode.com/users/" + eid
         name = requests.get(url).json().get("name")
         if name:
-            num_todos = len(requests.get(url).json())
-            completed = requests.get("{}/todos/".format(url),
+            num_todos = len(requests.get(url + "/todos/").json())
+            completed = requests.get(url + "/todos/",
                                      params={"completed": 'true'}).json()
 
             print("Employee {} is done with tasks({}/{}):"
