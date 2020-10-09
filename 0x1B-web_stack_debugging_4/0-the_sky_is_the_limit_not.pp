9293e9494312bf3c3763e3d935332985ade8b127
root <root@localhost.localdomain>
2020-10-08 19:29:17 -0500

    take 2

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,8 @@
-# uncomment ULIMIT in /etc/default/nginx and reload service
+# uncomment/add ULIMIT in /etc/default/nginx and reload service
 exec {'ulimit':
-  command => "sed -i '/^#ULIMIT/s/^#//' /etc/default/nginx && systemctl reload nginx",
-  path    => '/bin'
+  command => "sed -i '/^#ULIMIT.*4096/s/^#//' /etc/default/nginx;
+              grep -q ^ULIMIT.*4096 /etc/default/nginx ||
+              echo 'ULIMIT=\"-n 4096\"' >> /etc/default/nginx &&
+              systemctl restart nginx",
+  path    => ['/bin', '/usr/bin']
   }
