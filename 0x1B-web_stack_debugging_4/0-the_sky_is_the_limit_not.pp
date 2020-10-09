cbff1077d0f806b2b8f54272e2d77f5e1bba3392
Elcoz <oi@localhost.localdomain>
2020-10-09 15:23:33 -0500

    worker_processes

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
 # comment out ULIMIT in /etc/default/nginx and restart service
 exec {'ulimit':
-  command => "/bin/sed -i '/^ULIMIT/s/^/#/' /etc/default/nginx ; /usr/sbin/service restart nginx",
+  command => "/bin/sed -i '/worker_processes/s/ 4;$/ 1024;/' /etc/nginx/nginx.conf ; /usr/sbin/service restart nginx",
   }
