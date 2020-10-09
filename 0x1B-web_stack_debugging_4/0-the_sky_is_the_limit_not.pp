08843cebfcf211602a727334749f8ad731976da0
Elcoz <oi@localhost.localdomain>
2020-10-09 14:07:43 -0500

    all one line

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,4 @@
 # uncomment/add ULIMIT in /etc/default/nginx and reload service
 exec {'ulimit':
-  command => "/bin/sed -i '/^ULIMIT/s/^/#/' /etc/default/nginx &&
-              /bin/systemctl restart nginx",
+  command => "/bin/sed -i '/^ULIMIT/s/^/#/' /etc/default/nginx ; /bin/systemctl restart nginx",
   }
