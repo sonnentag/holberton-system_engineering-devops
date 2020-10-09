a309150921e079339ab70d5d18606ca8017b6587
root <root@localhost.localdomain>
2020-10-08 19:32:00 -0500

    reverting due to no change, needs ensure

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,6 @@
 # uncomment/add ULIMIT in /etc/default/nginx and reload service
 exec {'ulimit':
-  command => "sed -i '/^#ULIMIT.*4096/s/^#//' /etc/default/nginx;
-              grep -q ^ULIMIT.*4096 /etc/default/nginx ||
-              echo 'ULIMIT=\"-n 4096\"' >> /etc/default/nginx &&
+  command => "sed -i '/^#ULIMIT/s/^#//' /etc/default/nginx &&
               systemctl restart nginx",
-  path    => ['/bin', '/usr/bin']
+  path    => '/bin'
   }
